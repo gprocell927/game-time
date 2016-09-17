0210a270fd84f17aa0af93b26389d29b346ae3c8
Gabi Procell <gprocell927@gmail.com>
2016-09-17 11:24:06 -0600

    WIP added score and score test files

---
+++ test/score-test.js
@@ -0,0 +1,11 @@
+const assert = require('chai').assert;
+const Score = require("../lib/score.js");
+
+
+describe("Score", function () {
+
+  it('should be a function', function () {
+    assert.isFunction(Score);
+  });
+
+});
